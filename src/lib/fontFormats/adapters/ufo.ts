183485ed2b5cd7d82645d94ea82d0af80fea4ca3
Chiaki.C <akiakira02@gmail.com>
2026-06-18 22:43:00 +0800

    refactor(persistence): remove legacy UFO stores

--- src/lib/fontFormats/adapters/ufo.ts
+++ src/lib/fontFormats/adapters/ufo.ts
@@ -2,12 +2,10 @@ export {
   importUfoWorkspace,
   importUfoWorkspaceEntries,
   isRelevantUfoTextFile,
-  loadUfoProjectIntoFontData,
   pathToUfoContour,
   pickDefaultLayer,
   serializeGlifRecord,
   serializeXmlPlist,
-  syncHotFontDataToUfoRecords,
   type ImportedUfoWorkspace,
   type UfoWorkspaceEntry,
 } from 'src/lib/fontFormats/ufoFormat'
